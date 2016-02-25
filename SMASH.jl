4797c968d6f5c64e0ff8309bd1c9231ba6463b03
Jarvist Moore Frost <jarvist@gmail.com>
2016-02-25 17:49:27 +0000

    Rejig and put together proper SMASH module...

--- SMASH.jl
+++ SMASH.jl
@@ -1,17 +1,101 @@
-# S*M*A*S*H
+# S*M*A*S*H - module and XDATCAR Reader
 # Reconstituting the Glazer tilt notation for Perovskites from sampling molecular dynamics.
+# VASP (electronic structure package) XDATCAR (ab-initio molecular dynamics file format) reader
 #
 # Jarvist Moore Frost, University of Bath
-# File begun 2014-07-07
+# File begun 2014-07-12
 
-push!(LOAD_PATH,"./") # Temporary versions of modules in PWD
-using XDATCAR
+module SMASH 
 
-# Packages to pull in...
-#using Gaston    # interface to plot using GnuPlot
-                # I would use the MatPlotlib hooks; but this actually works
-#set_terminal("x11") # Rubbish installation on Jarv's Mac @ Work... - Keep it old school!
-using Plots # Generic 'Plots' module can run PyPlot, etc. as backend
+export atomic, Trajectory, read
+
+atomic=["H", "He", 
+"Li", "Be", "B", "C", "N", "O", "F", "Ne", 
+"Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", 
+"K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", 
+"Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te","I", "Xe", 
+"Cs","Ba",
+"La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu",
+"Hf","Ta","W","Re","Os","Ir","Pt", "Au", "Hg", "Tl", "Pb", "Bi","Po","At","Rn","Fr","Ra"]
+# indices are atomic number...
+
+# Print our (Atomic Number) table
+#for Z in 1:length(atomic)
+#    println(Z," ",atomic[Z])
+#end
+
+type Trajectory #NB: need to read moar on constructors...
+   cell
+   natoms::Int
+   frames
+   atomlookup::Array
+end
+
+function readnlines(f,n)
+    local lines=""
+    local i=1
+    for i=1:n
+        lines=lines*readline(f)
+    end
+    return (lines)
+end
+
+#readmatrix(f, nlines) = readdlm(IOBuffer(string([readline(f) for i in 1:nlines])))
+readmatrix(f, nlines) = readdlm(IOBuffer(readnlines(f,nlines)))
+
+# XDATCAR LOOKS LIKE THIS: 
+
+# Perovskite_MD
+#           1
+#    12.580170    0.000078   -0.039648
+#     0.000077   12.547782   -0.000152
+#    -0.039643   -0.000153   12.594017
+#   C    N    H    Pb   I
+#   8   8  48   8  24
+#Direct configuration=    50
+#   0.43568603  0.50228894  0.53798652
+#   0.03842720  0.49679247  0.48113604
+
+function read_XDATCAR(f::IOStream) 
+    t=Trajectory([],0,[],[]) 
+    
+    l=readline(f) #Title
+    l=readline(f) #Always a '1' ?
+    
+    t.cell=readdlm(IOBuffer(readnlines(f,3))) #Unit cell spec (3x3 matrix of basis vectors)
+    println(t.cell)
+
+#    atomlookup=readdlm(IOBuffer(readnlines(f,2)))
+    atomcrossref=readmatrix(f,2) # Ref to POTCAR; AtomName and #ofatoms
+
+#    println(atomlookup)
+
+    atoms=Int(sum(atomcrossref[2,1:end])) #Total atoms in supercell; quite ugly; but works
+    t.natoms=atoms
+
+    for atomtype = 1:length(atomcrossref[2,1:end]) # Each Atom... 
+        for i in 1:atomcrossref[2,atomtype] # For i number of each atoms
+            push!(t.atomlookup,indexin([atomcrossref[1,atomtype]],atomic)[1])
+        end
+    end
+    print(t.atomlookup)
+
+    println("$atoms atoms in XDATCAT frames")
+    #frames=readdlm(f , dlm=(r"\r?\n?",r"Direct configuration=?"))
+    #print(frames)
+    
+    nframe=0
+    while !eof(f) 
+        nframe=nframe+1
+
+        stepsizeline=readline(f)
+        push!(t.frames,readmatrix(f,atoms))   # Fractional coordinates!
+#        print(frame)
+    end
+    println("read_XDATCAR: $nframe Green Bottles...")
+
+    return t
+end
 
 # Print titles...
 function print_titles()
@@ -23,81 +107,15 @@ function print_titles()
     TILT=   Any["(+)","(-)","(0)"]
 
     println("Reconstituting the Glazer tilt notation for Perovskites from sampling molecular dynamics")
-    print("S*M*A*S*H: ") 
+    print("S*M*A*S*H: ")
     for WORDS in SMASH
         print(  WORDS[1+rand(Uint32)%length(WORDS)]," ",
                  TILT[1+rand(Uint32)%length( TILT)]," " )
     end
     println()
 end
-# Quite enough of that; let's get on with the real work... 
+# Quite enough of that; let's get on with the real work...
 
-function plot_octahedra() #Doesn't work currently...
-    figure(1)
-    c = Gaston.CurveConf()
-    c.plotstyle="pm3d"
-    for i=1:4
-        Xa,Ya,Za=0.0,0.0,0.0
-        Xb=i%3
-        Yb=i%2
-        Zb=i
-        gnuplot_send("set arrow $i from $Xa,$Ya,$Za to $Xb,$Yb,$Zb nohead") 
-    end
-    plot()
-end
-
-
-# MAIN
 print_titles()
-#plot_octahedra() #currently crashes out....
 
-
-# Test routines...
-f=open("testmd2-nonselective_XDATCAR","r")
-t=XDATCAR.read(f) #Returns type XDATCAR.Trajcetory
-
-println("OK; frames read...")
-println("Fractional coordinates; t.frames[123]")
-println(t.frames[123])
-
-println("Expanding to real coordinates; t.frames[123]*t.cell")
-println(t.frames[123]*t.cell)
-
-println("Dividing to fractional coordinates; t.frames[123]/t.cell")
-println(t.frames[123]/t.cell)
-
-SUPER=2
-
-Pb={}
-for i in 1:123
-    push!(Pb,mod((t.frames[i][65:72,:]*SUPER),1)*t.cell/SUPER)
-end
-println(Pb)
-
-println("STATS")
-println(mean(Pb))
-
-plot(t.frames[123][:,1],t.frames[123][:,2]) #,"plotstyle","linespoints")
-
-read(STDIN,Char) # wait for character before ending (and thus closing the GnuPlots)
-
-end
-
-println("Pb Extractor...")
-Pb={} # ToDo: This code doesn't work :) FIXME 
-v=hcat(t.frames[123],t.atomlookup) # pastes coords with atom #
-
-for a in v[:]
-    println(v," ") #,t.atomlookup[indexin(v,t.frames[123])])
-    if (a[4]==82)
-        push!(Pb,v)
-    end
-end
-
-println(Pb)
-#plot(Pb[:,1],Pb[:,2])
-
-plot(t.frames[123][:,1],t.frames[123][:,2],"plotstyle","points")
-
-read(STDIN,Char) # wait for character before ending (and thus closing the GnuPlots)
-
+end # Module
---
+++ oldSMASH.jl
@@ -0,0 +1,103 @@
+# S*M*A*S*H
+# Reconstituting the Glazer tilt notation for Perovskites from sampling molecular dynamics.
+#
+# Jarvist Moore Frost, University of Bath
+# File begun 2014-07-07
+
+push!(LOAD_PATH,"./") # Temporary versions of modules in PWD
+using XDATCAR
+
+# Packages to pull in...
+#using Gaston    # interface to plot using GnuPlot
+                # I would use the MatPlotlib hooks; but this actually works
+#set_terminal("x11") # Rubbish installation on Jarv's Mac @ Work... - Keep it old school!
+using Plots # Generic 'Plots' module can run PyPlot, etc. as backend
+
+# Print titles...
+function print_titles()
+    SMASH=Any[ Any["System","Systematic","Sub","Simulated","Standard","Symbiotic"],
+            Any["Method","Mash","Martian","Metrication","Molecular","Mutual"],
+            Any["Analysis","Analytic","Ability","And","Atomic","Aristotype"],
+            Any["Subtype","Suitable","Sublime","Subtle"],
+            Any["Holonomy","Homeotype","Hypothetic"]]
+    TILT=   Any["(+)","(-)","(0)"]
+
+    println("Reconstituting the Glazer tilt notation for Perovskites from sampling molecular dynamics")
+    print("S*M*A*S*H: ") 
+    for WORDS in SMASH
+        print(  WORDS[1+rand(Uint32)%length(WORDS)]," ",
+                 TILT[1+rand(Uint32)%length( TILT)]," " )
+    end
+    println()
+end
+# Quite enough of that; let's get on with the real work... 
+
+function plot_octahedra() #Doesn't work currently...
+    figure(1)
+    c = Gaston.CurveConf()
+    c.plotstyle="pm3d"
+    for i=1:4
+        Xa,Ya,Za=0.0,0.0,0.0
+        Xb=i%3
+        Yb=i%2
+        Zb=i
+        gnuplot_send("set arrow $i from $Xa,$Ya,$Za to $Xb,$Yb,$Zb nohead") 
+    end
+    plot()
+end
+
+
+# MAIN
+print_titles()
+#plot_octahedra() #currently crashes out....
+
+
+# Test routines...
+f=open("testmd2-nonselective_XDATCAR","r")
+t=XDATCAR.read(f) #Returns type XDATCAR.Trajcetory
+
+println("OK; frames read...")
+println("Fractional coordinates; t.frames[123]")
+println(t.frames[123])
+
+println("Expanding to real coordinates; t.frames[123]*t.cell")
+println(t.frames[123]*t.cell)
+
+println("Dividing to fractional coordinates; t.frames[123]/t.cell")
+println(t.frames[123]/t.cell)
+
+SUPER=2
+
+Pb={}
+for i in 1:123
+    push!(Pb,mod((t.frames[i][65:72,:]*SUPER),1)*t.cell/SUPER)
+end
+println(Pb)
+
+println("STATS")
+println(mean(Pb))
+
+plot(t.frames[123][:,1],t.frames[123][:,2]) #,"plotstyle","linespoints")
+
+read(STDIN,Char) # wait for character before ending (and thus closing the GnuPlots)
+
+end
+
+println("Pb Extractor...")
+Pb={} # ToDo: This code doesn't work :) FIXME 
+v=hcat(t.frames[123],t.atomlookup) # pastes coords with atom #
+
+for a in v[:]
+    println(v," ") #,t.atomlookup[indexin(v,t.frames[123])])
+    if (a[4]==82)
+        push!(Pb,v)
+    end
+end
+
+println(Pb)
+#plot(Pb[:,1],Pb[:,2])
+
+plot(t.frames[123][:,1],t.frames[123][:,2],"plotstyle","points")
+
+read(STDIN,Char) # wait for character before ending (and thus closing the GnuPlots)
+
