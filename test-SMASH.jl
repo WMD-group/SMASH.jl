499a6115a8755e73bf0a4b56e97bf1c464531171
Jarvist Moore Frost <jarvist@gmail.com>
2017-02-08 15:59:57 +0000

    More references / research.

--- test-SMASH.jl
+++ test-SMASH.jl
@@ -34,34 +34,39 @@ end
 # http://dx.doi.org/10.1006/jagm.1996.0062
 # But these slides (see page 17) are fairly understandable:
 # https://people.orie.cornell.edu/miketodd/ublndeta.pdf
+#
+# Cumby's Python implementation seems heavily influenced by Nina Moshtagh's Matlab impelementation:
+# https://uk.mathworks.com/matlabcentral/fileexchange/9542-minimum-volume-enclosing-ellipsoid
+# https://doi.org/10.1.1.116.7691
 function minimumVolumeEllipsoid(points; MaxCycles=20, tolerance=1e-6)
     println("minimumVolumeEllipsoid( $points, MaxCycles=$MaxCycles")
 
     N=6 #hard coded for now
-    D=2 # Number of dimensions? seems to affect step size; might be leading to numerical instability and solution collapse
+    D=3 # Number of dimensions? seems to affect step size; might be leading to numerical instability and solution collapse
 
-    Q=Array{Float64}(points)
-    Q=hcat(Q,ones(Q)) # I don#t really understand what this is doing - padding Q with '1.0's
-    println("Q: ")
-    display(Q)
+    X=Array{Float64}(points)
+    X=hcat(X,ones(X)) # I don't really understand what this is doing - padding Q with '1.0's
+    println("X: ")
+    display(X)
 
-    err = 1.0 + tolerance
+    err = 1e7
     u = zeros(N).+(1/N)
     display(u)
 
     while err>tolerance
-        V=Q*(diagm(u)*Q')
+        V=X*(diagm(u)*X')
         display(V)
-        M=diag(Q'*(inv(V)*Q))
+        M=diag(X'*(inv(V)*X))
         display(M)
 
 		j=indmax(M)
         maximum = M[j]
         println("\nindmax(M)=$j, maximum=M[j]=$maximum")
 
-        step_size = (maximum - D - 1) / ((D + 1) * (maximum - 1))
-        new_u = (1.0 - step_size) * u
-        new_u[j] += step_size
+        delta = (maximum - D - 1) / ((D + 1) * (maximum - 1))
+        println("\nDelta: $delta (should be >0)")
+        new_u = (1.0 - delta) * u
+        new_u[j] += delta
         display(new_u)
         err = norm(new_u - u)
         println("Err: $err")
